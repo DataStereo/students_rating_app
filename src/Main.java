66bf27561b3e74584089f7956d8483c66cbd374d
oleh_m <morozov89oleg@gmail.com>
2023-06-05 14:25:12 +0300

    added get average rating calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Student student1 = new Student("Student1");
+        Student student2 = new Student("Student2");
+        Student student3 = new Student("Student3");
+
+        System.out.println("Enter rating for " + student1.getName() + " > ");
+        int studentRating1 = scanner.nextInt();
+        student1.setRating(studentRating1);
+        System.out.println("Enter rating for " + student2.getName() + " > ");
+        int studentRating2 = scanner.nextInt();
+        student2.setRating(studentRating2);
+        System.out.println("Enter rating for " + student3.getName() + " > ");
+        int studentRating3 = scanner.nextInt();
+        student3.setRating(studentRating3);
+
+        System.out.println("Average rating for all students " + Student.getAvgRating());
     }
 }
